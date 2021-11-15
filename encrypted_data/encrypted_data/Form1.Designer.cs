a9dae5920e394150bb3020e3f5bc46fa8c403e63
berkantrl <65579418+berkantrl@users.noreply.github.com>
2021-11-15 14:41:50 +0300

    encrypted_data

---
+++ encrypted_data/encrypted_data/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace encrypted_data
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtnumber = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.txtsurname = new System.Windows.Forms.TextBox();
+            this.buttonsave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-6, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1014, 329);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(125, 29);
+            this.txtname.Multiline = true;
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(189, 41);
+            this.txtname.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(597, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Account Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(626, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Surname";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtnumber
+            // 
+            this.txtnumber.Location = new System.Drawing.Point(789, 99);
+            this.txtnumber.Multiline = true;
+            this.txtnumber.Name = "txtnumber";
+            this.txtnumber.Size = new System.Drawing.Size(189, 41);
+            this.txtnumber.TabIndex = 7;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(789, 29);
+            this.txtpassword.Multiline = true;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(189, 41);
+            this.txtpassword.TabIndex = 8;
+            // 
+            // txtmail
+            // 
+            this.txtmail.Location = new System.Drawing.Point(125, 161);
+            this.txtmail.Multiline = true;
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(189, 41);
+            this.txtmail.TabIndex = 9;
+            // 
+            // txtsurname
+            // 
+            this.txtsurname.Location = new System.Drawing.Point(125, 99);
+            this.txtsurname.Multiline = true;
+            this.txtsurname.Name = "txtsurname";
+            this.txtsurname.Size = new System.Drawing.Size(189, 41);
+            this.txtsurname.TabIndex = 10;
+            // 
+            // buttonsave
+            // 
+            this.buttonsave.Location = new System.Drawing.Point(590, 168);
+            this.buttonsave.Name = "buttonsave";
+            this.buttonsave.Size = new System.Drawing.Size(145, 45);
+            this.buttonsave.TabIndex = 11;
+            this.buttonsave.Text = "Save";
+            this.buttonsave.UseVisualStyleBackColor = true;
+            this.buttonsave.Click += new System.EventHandler(this.buttonsave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 564);
+            this.Controls.Add(this.buttonsave);
+            this.Controls.Add(this.txtsurname);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtnumber);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtnumber;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.TextBox txtsurname;
+        private System.Windows.Forms.Button buttonsave;
+    }
+}
+
---
+++ encrypted_data/encrypted_data/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace encrypted_data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-67U2HEQ\SQLEXPRESS;Initial Catalog=encryptdata; Integrated Security = True");
+        void list()
+        {
+            SqlDataAdapter data = new SqlDataAdapter("Select * From TBLDATAS", connection);
+            DataTable dataTable = new DataTable();
+            data.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void buttonsave_Click(object sender, EventArgs e)
+        {
+            string name = txtname.Text;
+            string encrypted_name = encrypt(name);
+            
+            string surname = txtsurname.Text;
+            string encrypted_surname = encrypt(surname);
+
+            string mail = txtmail.Text;
+            string encrypted_mail = encrypt(mail);
+
+            string password = txtpassword.Text;
+            string encrypted_password = encrypt(password);
+
+            string Anumber = txtnumber.Text;
+            string encrypted_Anumber = encrypt(Anumber);
+
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("Insert Into TBLDATAS (NAME, SURNAME, MAIL, PASSWORD, ACCOUNTNUMBER) values (@p1,@p2,@p3,@p4,@p5)", connection);
+            command.Parameters.AddWithValue("@p1", encrypted_name);
+            command.Parameters.AddWithValue("@p2", encrypted_surname);
+            command.Parameters.AddWithValue("@p3", encrypted_mail);
+            command.Parameters.AddWithValue("@p4", encrypted_password);
+            command.Parameters.AddWithValue("@p5", encrypted_Anumber);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Data Added");
+
+        }
+
+        public string encrypt(string data)
+        {
+
+            char[] data_array = data.ToCharArray();
+            string characters = "ABCÇDEFGHJKLIİJKLMNOÖPRSŞTUÜVYZWXQabcdefghıijklmnoöprsştuüvyzwxq!'+-.,";
+            char[] character = characters.ToCharArray();
+            string[] elementsymbols = {"He","Li","Be","Ne","Na", "Mg", "Al","Si","Cl","Ar","Ca","Sc","Ti","Cr", "Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb",
+                "Sr","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+                "Hf","Ta","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi"};
+            string encrytpdata = "";
+            for(int i = 0; i < data_array.Length; i++) { 
+                for (int j = 0; j < characters.Length; j++)
+                {
+                    if (data_array[i] == characters[j]){
+                        encrytpdata += elementsymbols[j];
+
+
+                    }
+                }
+                data = encrytpdata;
+            }
+            return data;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list();
+        }
+    }
+}
